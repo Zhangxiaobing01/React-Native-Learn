8230bde684fa88f99091b13cc0402763d61b521f
zhangxiaobing <zhangxiaobing@zhangxiaobingdemacbook-pro.local>
2016-02-26 17:38:28 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by zhangxiaobing on 16/2/26.
+//  Copyright © 2016年 zhangxiaobing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by zhangxiaobing on 16/2/26.
+//  Copyright © 2016年 zhangxiaobing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
